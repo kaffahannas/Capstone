9b0f5e63114b15158c227c6509cd3c02dc73fc52
Vincent Julian <vincentjulianhadisantoso@gmail.com>
2026-02-21 22:52:14 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LightenUp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
